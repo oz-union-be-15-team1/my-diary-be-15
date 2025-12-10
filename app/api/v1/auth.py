26f8f861288574fccbbadea668e251a1316bdf08
god-gy <god.gy0321@gmail.com>
2025-12-10 14:18:14 +0900

    ⭐feat: Add Features -> user(logout)

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends
-from app.schemas.user import UserCreate, UserLogin, UserResponse
+from fastapi import APIRouter, Depends, HTTPException
+
+from app.models.user import User
+from app.schemas.user import UserCreate, UserLogin, UserResponse, LoginResponse
 from app.services.auth_service import AuthService
-from app.core.security import get_current_user
+from app.core.security import get_current_user, create_access_token, oauth2_scheme
+from app.services.auth_service import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
-
 
 @router.post("/register", response_model=UserResponse, description="register new user")
 async def register_user(payload: UserCreate):
@@ -15,13 +17,23 @@ async def register_user(payload: UserCreate):
     )
     return user
 
-
-@router.post("/login", response_model=UserResponse, description="login user")
+@router.post("/login", response_model=LoginResponse, description="login user")
 async def login(payload: UserLogin):
     user = await AuthService.authenticate(payload.username, payload.password)
-    return user
-
+    token = create_access_token(str(user.id))
+    return {
+        "access_token": token,
+        "user": user
+    }
 
 @router.get("/me", response_model=UserResponse, description="get user info")
 async def get_me(user=Depends(get_current_user)):
     return user
+
+@router.post("/logout", description="logout user")
+async def logout(
+    current_user: User = Depends(get_current_user),
+    token: str = Depends(oauth2_scheme)
+):
+    await AuthService.logout(token, current_user)
+    return {"detail": "Successfully logged out"}
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel, Field
 
-
 class UserCreate(BaseModel):
     username: str = Field(min_length=3, max_length=32)
     password: str = Field(min_length=3, max_length=32)
@@ -9,7 +8,6 @@ class UserCreate(BaseModel):
 class UserLogin(BaseModel):
     username: str
     password: str
-    email: str
 
 class UserResponse(BaseModel):
     id: int
@@ -18,3 +16,8 @@ class UserResponse(BaseModel):
 
     class Config:
         from_attributes = True
+        orm_mode = True
+
+class LoginResponse(BaseModel):
+    access_token: str
+    user: UserResponse
